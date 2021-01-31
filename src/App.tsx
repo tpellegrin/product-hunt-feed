9e25bd4b2d4106fcefa0ec350371997978b2c478
tpellegrin <tgpellegrin@gmail.com>
2021-01-30 22:00:20 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import GlobalStyle from './styles/GlobalStyle';
+import Navbar from './components/Navbar';
+import Posts from './components/pages/Posts';
 
 function App() {
   return (
     <Fragment>
-      <GlobalStyle />
-
-      <div className="App"></div>
+      <GlobalStyle ignore='Navbar' />
+      
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route
+            path='/popular'
+            component={() => <Posts orderBy='VOTES' />}
+          />
+          <Route
+            path='/newest'
+            component={() => <Posts orderBy='NEWEST' />}
+          />
+        </Switch>
+      </Router>
     </Fragment>
   );
 }
